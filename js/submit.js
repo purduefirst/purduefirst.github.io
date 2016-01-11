e7d68a22f9cf1f2aa6daeadc0e9393fc7d25a4e2
samontea <merciers.merciers@gmail.com>
2016-01-10 23:44:38 -0500

    contact us what not

---
+++ js/submit.js
@@ -0,0 +1,17 @@
+function submitMailTo( email ) {
+    var first = document.getElementById("first").value;
+    var last = document.getElementById("last").value;
+    var subject = document.getElementById("subject").value;
+    var message = document.getElementById("message").value;
+
+    var url = 'mailto:' + form.getAttribute("contact-addr") + '?subject=' + first + ' ' + last + ': ' + subject + '&body=' + message;
+
+	alert(url);
+
+    var win = window.open(url, '_blank');
+    win.focus();
+}
+
+
+var form = document.getElementById("contact-us");
+form.addEventListener("submit", submitMailTo);
